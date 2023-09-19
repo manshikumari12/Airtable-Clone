2aa087c7e2a0deff355657d584da8e19252a5469
Manshi Kumari <119393324+manshikumari12@users.noreply.github.com>
2023-09-19 23:17:24 +0530

    ok

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(cors())
 app.use(express.json());
 
 app.use("/", UserRouter);
-app.use("/", PostRouter);
+app.use("/post", PostRouter);
 app.get("/", (req, res) => {
   res.send("Home Page");
 });
--- backend/route/post.route.js
+++ backend/route/post.route.js
@@ -1,34 +1,19 @@
 const express = require("express");
-const { authenticate } = require("../middleware/authenticate.middleware");
+
 const { PostModel } = require("../model/post.model");
 const PostRouter = express.Router();
 
- PostRouter.get("/", async (req,res)=>{
-    const {device,device1,device2}=req.query
-    try {
-        let query = {};
-    
-        if (device) {
-          query.device = { $regex: new RegExp(device, "i") };
-        }
-    
-        if (device1 && device2) {
-            const regexStr = device1 + '|' + device2;
-            query.device = { $regex: new RegExp(regexStr, "i") };
-        // query.device = {$in: [device1,device2]};
-        }
-       
-    
-        const data = await PostModel.find(query);
-        res.send(data);
-      } catch (err) {
-        console.log(err);
-        res.status(500).send(err.message);
-      }
+ PostRouter.get("/add", async (req,res)=>{
+   try {
+    const alldata = await PostModel.find()
+    res.json(alldata)
+   } catch (error) {
+    console.log(error)
+   }
   })
   
   
-  // add the posts of perticular user /posts/add
+
   
   PostRouter.post("/add", async (req, res) => {
       const payload = req.body;
@@ -48,24 +33,17 @@ const PostRouter = express.Router();
   
   
     PostRouter.patch("/update/:id", async (req,res) =>{
-      const id = req.params.id;
-      const change = req.body;
-    
-      const note = await PostModel.findOne({ _id: id });
-      const user_id_in_posts = note.userID;
-      const user_id_req = req.body.userID;
-    
+      const { id } = req.params;
+      const payload = req.body;
       try {
-        if (user_id_req !== user_id_in_posts) {
-          res.send({ msg: "you are not authorized" });
-        } else {
-          await PostModel.findByIdAndUpdate({ _id: id }, change);
-          res.send("update the data");
-        }
+        const update=  await PostModel.findByIdAndUpdate({ _id: id }, payload);
+        await update.save()
+          res.status(200).send({ msg: " data updated successfully!",update })
       } catch (error) {
-        res.send({ msg: "something went wrong", error: error.message });
-        console.log(error)
+        console.error('Error deleting card:', error);
+          res.status(500).send({ msg: error.message })
       }
+
   })
   
   
@@ -73,25 +51,14 @@ const PostRouter = express.Router();
   
   
   PostRouter.delete("/delete/:id", async (req,res) =>{
-      const id = req.params.id;
-      const change = req.body;
-    
-      const note = await PostModel.findOne({ _id: id });
-      const user_id_in_posts = note.userID;
-      const user_id_req = req.body.userID;
-    
-      try {
-        if (user_id_req !== user_id_in_posts) {
-          res.send({ msg: "you are not authorized" });
-        } else {
-          const posts=await PostModel.findByIdAndDelete({ _id: id }, change)
-         
-          res.send("msg delete");
-        }
-      } catch (error) {
-        res.send({ msg: "something went wrong", error: error.message });
-        console.log(error)
-      }
+    const Id = req.params.id;
+    try {
+        await PostModel.findByIdAndDelete(Id);
+        res.json({ success: true, message: 'Card deleted successfully.' });
+    } catch (error) {
+        console.error('Error deleting card:', error);
+        res.status(500).json({ success: false, error: 'An error occurred while deleting card.' });
+    }
   })
 
     //maximum no. of Comment keep the route as /posts/top
