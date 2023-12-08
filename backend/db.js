2a6b28dccfc08c57c9f45b130e85f6fbf123f08c
Manshi Kumari <119393324+manshikumari12@users.noreply.github.com>
2023-12-08 20:25:45 +0530

    done

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const env= require("dotenv")
 env.config()
 
-const connection = mongoose.connect("mongodb://127.0.0.1:27017/code");
+const connection = mongoose.connect("mongodb+srv://manshisbp:manshi@cluster0.vildgct.mongodb.net/taskmain?retryWrites=true&w=majority");
 
 module.exports = {
   connection,
